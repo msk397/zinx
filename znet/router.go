2992e4fb26ed8bbaefda83bbaa920f7efbe899d9
msk397 <machangxinq@gmail.com>
2023-08-13 09:21:49 +0800

    完成V0.9版本，实现连接管理

--- znet/router.go
+++ znet/router.go
@@ -7,7 +7,7 @@ import (
 // 实现 Router 时，先嵌入这个基类，然后根据需要对这个基类的方法进行重写就行了
 type BaseRouter struct{}
 
-// 这里之所以 BaseRouter 的方法都为空
+// PreHandle 这里之所以 BaseRouter 的方法都为空
 // 是因为有的 Router 不希望有 PreHandle 或 PostHandle
 // 所以 Router 全部继承 BaseRouter 的好处就是，不需要实现 PreHandle 和 PostHandle 也可以将IRouter实例化
 func (b *BaseRouter) PreHandle(request ziface.IRequest) {}
